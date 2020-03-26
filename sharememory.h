566182dbd5a063c70fe3ae3870b78b695f8049f7
smg8n <60581192+smg8n@users.noreply.github.com>
2020-03-26 12:18:36 +0530

    Update sharememory.h

--- sharememory.h
+++ sharememory.h
@@ -1,1 +1,29 @@
 
+#ifndef SHAREMEMORY_H
+#define SHAREMEMORY_H
+
+typedef struct
+{
+    unsigned int secs;
+    unsigned int nans;
+} simclock;
+
+typedef struct
+{
+    int pclass;
+    int pids;
+    int fpid;
+    int priority;
+    simclock smcputime;
+    simclock smsystime;
+    simclock smblktime;
+    simclock smwaittime;
+} pcblock;
+
+typedef struct
+{
+    pcblock pctable[18];
+    simclock simtime; 
+} shmem;
+
+#endif /* SHAREMEMORY_H */
